78d876a690cf0e5fc185e26cd044b206c379c62d
Suraj Salavi <surajsalavi4@gmail.com>
2022-05-28 16:02:07 +0530

    Day 11 completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:second/pages/login_page.dart';
 import 'pages/home_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main()
-{
+void main() {
   runApp(MyApp());
 }
 
@@ -14,23 +13,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "My App",
-      theme: new ThemeData(
-        primarySwatch: Colors.teal,
-        accentColor: Colors.red,
-        brightness: Brightness.light,
-        fontFamily: GoogleFonts.lato().fontFamily
-      ),
-
+      title: "My App",
+      theme: ThemeData(
+          primarySwatch: Colors.teal,
+          accentColor: Colors.red,
+          brightness: Brightness.light,
+          fontFamily: GoogleFonts.lato().fontFamily),
       routes: {
-          "/":(context)=>HomePage(),
-        "/home":(context)=>HomePage(),
-        "/login":(context)=>Login()
+        "/": (context) => HomePage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => Login()
       },
     );
   }
 }
-
-
-
-
